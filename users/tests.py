c0cd79199c89d97efe9b7cbcd6ce37b0672e3069
ashyrbaew <ashyrbaew@gmail.com>
2024-09-30 14:16:26 +0600

    * added bsing nginx configs * created TEST cases

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from users.models import User
+from decimal import Decimal
 
-# Create your tests here.
+
+class UserModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testuser', balance=100.00)
+
+    def test_user_creation(self):
+        self.assertEqual(self.user.username, 'testuser')
+        self.assertEqual(self.user.balance, Decimal('100.00'))
+
+    def test_update_balance(self):
+        self.user.update_balance(Decimal('50.00'))
+        self.assertEqual(self.user.balance, Decimal('150.00'))
+
+        self.user.update_balance(Decimal('-25.00'))
+        self.assertEqual(self.user.balance, Decimal('125.00'))
+
